5bc49043bae25b5f0ede122b577d222e6135cb25
Xinyi <Mingtianc8@gmail.com>
2025-08-06 15:16:52 -0700

    Added Button

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -10,13 +10,24 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            
             Text("What is your name?")
                 .font(.title)
+            
             TextField("Type your name here...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
                 .multilineTextAlignment(.center)
-                .font(title)
-            border(Color.)
+                .font(.title)
+                .border(Color.gray, width: 1)
+            
+            Button("Submit Name") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
+
         }
+        .padding(30)
     }
 }
 
